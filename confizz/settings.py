dabe9fe10ef48cdc79e9cd47bcfc2cd9d70daa34
shiv-0101 <shivendrathakur321@outlook.com>
2025-11-02 00:25:26 +0530

    Secure API keys and secrets by moving to .env and updating .gitignore

--- confizz/settings.py
+++ confizz/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t&g8ax^+*ext%gswzfp5#fpzak_2=y8n=d($)78)cr+8xhe5dg'
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-t&g8ax^+*ext%gswzfp5#fpzak_2=y8n=d($)78)cr+8xhe5dg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
